83262ca2ccce4ce57eee82e3aa4413a3715357af
als92 <senkinas@gmail.com>
2022-03-18 09:53:01 +0000

    your-order-please

---
+++ 6kyu/Your-Order,-Please.js
@@ -0,0 +1,9 @@
+// Problem link: https://www.codewars.com/kata/55c45be3b2079eccff00010f
+
+// Solution
+function order(words){
+  return words
+    .split(' ')
+    .sort((a,b)=>a.match(/\d/)-b.match(/\d/))
+    .join(' ')
+}
