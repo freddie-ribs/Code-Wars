942bbc34536101bbc50b75c6030d8bee0a970f8c
als92 <senkinas@gmail.com>
2022-03-29 09:20:53 +0100

    is triangle

---
+++ 7kyu/Is-Triangle.js
@@ -0,0 +1,7 @@
+// Problem link: https://www.codewars.com/kata/56606694ec01347ce800001b
+
+// Solution:
+
+function isTriangle(a,b,c) {
+  return a + b > c && a + c > b && b + c > a ? true : false
+}
