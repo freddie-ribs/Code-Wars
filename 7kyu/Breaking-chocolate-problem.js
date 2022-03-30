135581aff70f54160392c4b3b222acd62cedcba9
als92 <senkinas@gmail.com>
2022-03-30 10:34:10 +0100

    breaking chocolate problem

---
+++ 7kyu/Breaking-chocolate-problem.js
@@ -0,0 +1,10 @@
+// Problem link: https://www.codewars.com/kata/534ea96ebb17181947000ada
+
+// Solution: 
+
+function breakChocolate(n,m) {
+if ( n <= 0 || m <= 0 ) {
+return 0;
+}
+  return n * m - 1;
+}
