e9540ecd1f52963c2c0d9559734f1c8d4fdda827
als92 <senkinas@gmail.com>
2022-04-10 22:06:44 +0100

    fix-string-case

---
+++ 7kyu/Fix-string-case.js
@@ -0,0 +1,17 @@
+// Problem link: https://www.codewars.com/kata/5b180e9fedaa564a7000009a
+
+// Solution
+
+function solve(s){
+    let lowerC = 0;
+    let upperC = 0;
+    for( let i = 0;i<s.length;i++){
+      if( s[i] == s[i].toUpperCase()){
+        upperC++;
+      }
+      else{
+        lowerC++;
+      }
+    }
+    return lowerC >= upperC ? s.toLowerCase() : s.toUpperCase()
+}
