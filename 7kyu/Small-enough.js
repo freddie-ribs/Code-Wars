e667f5361c99e885a0ee283b9f09420aa4c5dbc7
als92 <senkinas@gmail.com>
2022-04-13 18:59:39 +0100

    small enough

---
+++ 7kyu/Small-enough.js
@@ -0,0 +1,12 @@
+// Problem link: https://www.codewars.com/kata/57cc981a58da9e302a000214
+
+// Solution:
+
+function smallEnough(a, limit) {
+  for (let i = 0; i < a.length; i++) {
+    if (a[i] > limit) {
+      return false
+    }
+  }
+  return true
+}
